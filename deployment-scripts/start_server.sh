52784c6e0c52346f0e2e1d151aff24bd5ae56b5a
Harold Reyes <eharoldreyes@gmail.com>
2015-10-27 15:15:06 +0800

    repaired .sh files

--- deployment-scripts/start_server.sh
+++ deployment-scripts/start_server.sh
@@ -3,10 +3,4 @@
 cd /home/ubuntu/server/
 sudo npm install
 sudo start webserver
-
-# Here is where you'd want to start your http daemon. For example:
-#service httpd start
-#exit $?
-
-# In this case, since it's just a placeholder, we don't need to do anything.
 exit 0
--- deployment-scripts/stop_server.sh
+++ deployment-scripts/stop_server.sh
@@ -2,10 +2,4 @@
 
 cd /home/ubuntu/server/
 sudo stop webserver
-
-# Here is where you'd want to stop your http daemon. For example:
-#service httpd stop
-#exit $?
-
-# In this case, since it's just a placeholder, we don't need to do anything.
 exit 0
